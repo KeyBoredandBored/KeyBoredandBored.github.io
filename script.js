e0672baa4e56b332ff7c1949c1c9a0c486674fdb
KeyBoredandBored <93796575+KeyBoredandBored@users.noreply.github.com>
2025-02-14 15:55:55 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function sayYes() {
+    document.body.innerHTML = "<h1>YAY! 💖 See you on Valentine’s Day! 😊</h1>";
+}
+
+function askAgain() {
+    document.querySelector(".buttons").classList.add("hidden");
+    document.getElementById("noResponse").classList.remove("hidden");
+}
+
+function submitReason() {
+    let reason = document.getElementById("reason").value;
+    if (reason.trim() === "") {
+        alert("Please give a reason! 😢");
+        return;
+    }
+    document.getElementById("sadGif").src = "https://media.giphy.com/media/3ohjUMLXhMgnEf1bS4/giphy.gif"; // Changes to another GIF
+    setTimeout(() => {
+        alert("Hmm... try again! 😏");
+        location.reload();
+    }, 3000);
+}
